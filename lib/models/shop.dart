e6b625040c19bb27e5936f523feb8f39f60a1829
loinp <nploi1998@gmail.com>
2019-11-21 04:21:00 +0700

    Remove comment line

--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -26,14 +26,12 @@ class Shop {
       this.imageBig});
 
   Shop.fromPoi(Poi poi) {
-//    name = String.fromCharCodes(Runes(poi.title));
     name = poi.title != null
         ? poi.title.replaceAll("<b>", '').replaceAll("</b>", '')
         : "";
     hash = poi.hash;
     latitude = poi.gps.latitude;
     longitude = poi.gps.longitude;
-//    address = String.fromCharCodes(Runes(poi.address));
     address = poi.address != null
         ? poi.address.replaceAll("<b>", '').replaceAll("</b>", '')
         : "";
