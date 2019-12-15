715e0a0da9f7f159036cdb7d0d398abc62c16a56
loinp <nploi1998@gmail.com>
2019-12-15 14:50:29 +0700

    Add refresh for shop detail

--- lib/screens/shop_detail_screen.dart
+++ lib/screens/shop_detail_screen.dart
@@ -32,10 +32,10 @@ class _ShopDetailScreenState extends State<ShopDetailScreen> {
   @override
   void initState() {
     super.initState();
-    _bloc = ShopDetailBloc()..add(ShopDetailByHashEvent(widget.shop.hash));
-    _feedbackBloc = FeedbackBloc()
-      ..add(FeedbackGetListFeedbackEvent(widget.shop.hash));
+    _bloc = ShopDetailBloc();
+    _feedbackBloc = FeedbackBloc();
     _authenticationBloc = AuthenticationBloc();
+    _refresh();
     shop = widget.shop;
   }
 
@@ -46,8 +46,7 @@ class _ShopDetailScreenState extends State<ShopDetailScreen> {
       bloc: _feedbackBloc,
       listener: (context, state) {
         if (state is FeedbackAddedFeedbackState) {
-          _bloc.add(ShopDetailByHashEvent(widget.shop.hash));
-          _feedbackBloc.add(FeedbackGetListFeedbackEvent(widget.shop.hash));
+          _refresh();
         }
       },
       child: BlocBuilder<ShopDetailBloc, ShopDetailState>(
@@ -78,180 +77,188 @@ class _ShopDetailScreenState extends State<ShopDetailScreen> {
                             ),
                           )));
                 }
-                return CustomScrollView(
-                  slivers: <Widget>[
-                    SliverAppBar(
-                      expandedHeight: MediaQuery.of(context).size.height * 0.3,
-                      pinned: true,
-                      flexibleSpace: FlexibleSpaceBar(
-                        background: Hero(
-                          tag: widget.shop.name + "-" + widget.shop.address,
-                          child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            child: CachedNetworkImage(
-                              imageUrl: widget.shop.imageBig,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        collapseMode: CollapseMode.pin,
-                        title: Text(widget.shop.name),
-                      ),
-                    ),
-                    SliverPersistentHeader(
-                      delegate: SliverAppBarDelegate(
-                        minHeight: 60.0,
-                        maxHeight: 60.0,
-                        child: ListTile(
-                          title: Text(widget.shop.address),
-                          trailing: SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.06,
-                            child: FloatingActionButton(
-                              heroTag: null,
-                              child: Icon(Icons.directions),
-                              onPressed: () async {
-                                final url =
-                                    "https://www.google.com/maps/dir/Current+Location/${widget.shop.latitude},${widget.shop.longitude}";
-                                if (await canLaunch(url)) {
-                                  await launch(url);
-                                }
-                              },
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    widget.shop.phoneNumber.isNotEmpty
-                        ? SliverPersistentHeader(
-                            delegate: SliverAppBarDelegate(
-                              minHeight: 60.0,
-                              maxHeight: 60.0,
-                              child: ListTile(
-                                title: Text(widget.shop.phoneNumber),
-                                trailing: SizedBox(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.06,
-                                  child: FloatingActionButton(
-                                    heroTag: null,
-                                    child: Icon(Icons.call),
-                                    onPressed: () async {
-                                      final items =
-                                          widget.shop.phoneNumber.split(";");
-                                      if (items.length > 1) {
-                                        _selectPhoneModalBottomSheet(
-                                            context, items);
-                                        return;
-                                      }
-                                      final url =
-                                          "tel:${widget.shop.phoneNumber}";
-                                      if (await canLaunch(url)) {
-                                        await launch(url);
-                                      }
-                                    },
-                                  ),
-                                ),
+                return RefreshIndicator(
+                  onRefresh: () async {
+                    _refresh();
+                    await Future.delayed(Duration(milliseconds: 400));
+                  },
+                  child: CustomScrollView(
+                    slivers: <Widget>[
+                      SliverAppBar(
+                        expandedHeight:
+                            MediaQuery.of(context).size.height * 0.3,
+                        pinned: true,
+                        flexibleSpace: FlexibleSpaceBar(
+                          background: Hero(
+                            tag: widget.shop.name + "-" + widget.shop.address,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              child: CachedNetworkImage(
+                                imageUrl: widget.shop.imageBig,
+                                fit: BoxFit.cover,
                               ),
                             ),
-                          )
-                        : SliverToBoxAdapter(),
-                    SliverPersistentHeader(
-                      delegate: SliverAppBarDelegate(
-                        minHeight: 60.0,
-                        maxHeight: 60.0,
-                        child: Center(
-                            child: Column(
-                          children: <Widget>[
-                            Text(S.of(context).rateAndReviewTitle),
-                            Hero(
-                              child: RatingBar(
-                                ratingWidget: RatingWidget(
-                                    full: Icon(
-                                      Icons.star,
-                                      color: Colors.amberAccent,
-                                    ),
-                                    empty: Icon(
-                                      Icons.star_border,
-                                      color: Colors.amberAccent,
-                                    ),
-                                    half: Icon(
-                                      Icons.star_half,
-                                      color: Colors.amberAccent,
-                                    )),
-                                onRatingUpdate: (double value) {
-                                  _authenticationBloc.user.then((user) {
-                                    if (user.fullName != null) {
-                                      Navigator.of(context).pushNamed(
-                                        ReviewScreen.routeName,
-                                        arguments: [
-                                          value,
-                                          widget.shop,
-                                          _feedbackBloc
-                                        ],
-                                      );
-                                    } else {
-                                      Navigator.of(context).pushNamed(
-                                        SignInScreen.routeName,
-                                      );
-                                    }
-                                  });
+                          ),
+                          collapseMode: CollapseMode.pin,
+                          title: Text(widget.shop.name),
+                        ),
+                      ),
+                      SliverPersistentHeader(
+                        delegate: SliverAppBarDelegate(
+                          minHeight: 60.0,
+                          maxHeight: 60.0,
+                          child: ListTile(
+                            title: Text(widget.shop.address),
+                            trailing: SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.06,
+                              child: FloatingActionButton(
+                                heroTag: null,
+                                child: Icon(Icons.directions),
+                                onPressed: () async {
+                                  final url =
+                                      "https://www.google.com/maps/dir/Current+Location/${widget.shop.latitude},${widget.shop.longitude}";
+                                  if (await canLaunch(url)) {
+                                    await launch(url);
+                                  }
                                 },
-                                itemCount: 5,
-                                allowHalfRating: true,
                               ),
-                              tag: "Rating",
-                            ),
-                          ],
-                        )),
-                      ),
-                    ),
-                    SliverToBoxAdapter(
-                      child: SizedBox(
-                        height: 20,
-                      ),
-                    ),
-                    SliverPersistentHeader(
-                      pinned: true,
-                      delegate: SliverAppBarDelegate(
-                        minHeight: 50.0,
-                        maxHeight: 50.0,
-                        child: Material(
-                          child: IntrinsicHeight(
-                            child: Row(
-                              children: <Widget>[
-                                Expanded(
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      Text(shop.rating.toStringAsFixed(2)),
-                                      Icon(Icons.star),
-                                    ],
-                                  ),
-                                ),
-                                VerticalDivider(),
-                                Expanded(
-                                    child: Center(
-                                        child: Text(
-                                            "${widgets.length} ${S.of(context).reviewsTitle}"))),
-                              ],
                             ),
                           ),
-                          elevation: 1,
                         ),
                       ),
-                    ),
-                    SliverFixedExtentList(
-                      itemExtent: 60.0,
-                      delegate: SliverChildListDelegate(
-                        widgets,
+                      widget.shop.phoneNumber.isNotEmpty
+                          ? SliverPersistentHeader(
+                              delegate: SliverAppBarDelegate(
+                                minHeight: 60.0,
+                                maxHeight: 60.0,
+                                child: ListTile(
+                                  title: Text(widget.shop.phoneNumber),
+                                  trailing: SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.06,
+                                    child: FloatingActionButton(
+                                      heroTag: null,
+                                      child: Icon(Icons.call),
+                                      onPressed: () async {
+                                        final items =
+                                            widget.shop.phoneNumber.split(";");
+                                        if (items.length > 1) {
+                                          _selectPhoneModalBottomSheet(
+                                              context, items);
+                                          return;
+                                        }
+                                        final url =
+                                            "tel:${widget.shop.phoneNumber}";
+                                        if (await canLaunch(url)) {
+                                          await launch(url);
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          : SliverToBoxAdapter(),
+                      SliverPersistentHeader(
+                        delegate: SliverAppBarDelegate(
+                          minHeight: 60.0,
+                          maxHeight: 60.0,
+                          child: Center(
+                              child: Column(
+                            children: <Widget>[
+                              Text(S.of(context).rateAndReviewTitle),
+                              Hero(
+                                child: RatingBar(
+                                  ratingWidget: RatingWidget(
+                                      full: Icon(
+                                        Icons.star,
+                                        color: Colors.amberAccent,
+                                      ),
+                                      empty: Icon(
+                                        Icons.star_border,
+                                        color: Colors.amberAccent,
+                                      ),
+                                      half: Icon(
+                                        Icons.star_half,
+                                        color: Colors.amberAccent,
+                                      )),
+                                  onRatingUpdate: (double value) {
+                                    _authenticationBloc.user.then((user) {
+                                      if (user.fullName != null) {
+                                        Navigator.of(context).pushNamed(
+                                          ReviewScreen.routeName,
+                                          arguments: [
+                                            value,
+                                            widget.shop,
+                                            _feedbackBloc
+                                          ],
+                                        );
+                                      } else {
+                                        Navigator.of(context).pushNamed(
+                                          SignInScreen.routeName,
+                                        );
+                                      }
+                                    });
+                                  },
+                                  itemCount: 5,
+                                  allowHalfRating: true,
+                                ),
+                                tag: "Rating",
+                              ),
+                            ],
+                          )),
+                        ),
                       ),
-                    ),
-                    SliverToBoxAdapter(
-                      child: SizedBox(
-                        height: MediaQuery.of(context).size.height,
+                      SliverToBoxAdapter(
+                        child: SizedBox(
+                          height: 20,
+                        ),
                       ),
-                    ),
-                  ],
+                      SliverPersistentHeader(
+                        pinned: true,
+                        delegate: SliverAppBarDelegate(
+                          minHeight: 50.0,
+                          maxHeight: 50.0,
+                          child: Material(
+                            child: IntrinsicHeight(
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(shop.rating.toStringAsFixed(2)),
+                                        Icon(Icons.star),
+                                      ],
+                                    ),
+                                  ),
+                                  VerticalDivider(),
+                                  Expanded(
+                                      child: Center(
+                                          child: Text(
+                                              "${widgets.length} ${S.of(context).reviewsTitle}"))),
+                                ],
+                              ),
+                            ),
+                            elevation: 1,
+                          ),
+                        ),
+                      ),
+                      SliverFixedExtentList(
+                        itemExtent: 60.0,
+                        delegate: SliverChildListDelegate(
+                          widgets,
+                        ),
+                      ),
+                      SliverToBoxAdapter(
+                        child: SizedBox(
+                          height: MediaQuery.of(context).size.height,
+                        ),
+                      ),
+                    ],
+                  ),
                 );
               },
             );
@@ -279,6 +286,11 @@ class _ShopDetailScreenState extends State<ShopDetailScreen> {
             ),
           );
         });
+  }
+
+  void _refresh() {
+    _bloc.add(ShopDetailByHashEvent(widget.shop.hash));
+    _feedbackBloc.add(FeedbackGetListFeedbackEvent(widget.shop.hash));
   }
 
   @override
